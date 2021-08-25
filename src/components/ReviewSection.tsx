bff3a4f4ab5ac636395d1ec7e9f3ae04311691e6
lxbraga <lucasgsbra@gmail.com>
2021-08-25 14:36:10 -0300

    feat: Add filename to form + handle some minor errors

--- src/components/ReviewSection.tsx
+++ src/components/ReviewSection.tsx
@@ -8,8 +8,12 @@ import moment from "moment";
 
 const ReviewSection: FC = () => {
 
-    const [review, setReview] = useState({ name: '', email: '', date: '', time: '' });
+    const [review, setReview] = useState({ name: '', email: '', date: '', time: '', filename: '' });
     const [file, setFile] = useState(null);
+	
+	const displayNone = {
+		display: 'none',
+	} as const;
 
     const changeText = (value: string, prop: string) => {
         const obj = { ...review };
@@ -27,16 +31,18 @@ const ReviewSection: FC = () => {
 
     async function sendEmail(e: any) {
         e.preventDefault();
-
         if (review.name === '' || review.email === '') {
             alert("You must fill all the fields");
             return;
         }
-        uploadFile(file);
-        emailjs.sendForm('service_7d7mjyh', 'template_06sj7t4', e.target, 'user_AtVqPPa3dfx6Y9RN4fJe4')
+		if (file !== null && file !== undefined){
+			uploadFile(file);
+		}
+        emailjs.sendForm('service_4f2hs4j', 'template_fu0ys7k', e.target, 'user_jDzpWECnca4Pv0olIIrfW')
         .then((result) => {
             console.log(result.text);
-            setReview({ name: '', email: '', date: '', time: '' });
+            setReview({ name: '', email: '', date: '', time: '', filename: '' });
+			setFile(null);
             alert("Your file has been successfully uploaded and you will receive an email with the date and time of the appointment you have booked")
         }, (error) => {
             console.log(error.text);
@@ -48,11 +54,11 @@ const ReviewSection: FC = () => {
     };
 
     function displayFileChoosen(file: any) {
-        if (file === null) {
-            return <p></p>;
-        };
-        if (file.name !== null)
-            return <p className="currentFile">{file.name}</p>;
+        if (file !== null && file !== undefined){
+			return <p className="currentFile">{file.name}</p>;
+		}else {
+			return <p></p>;
+		}
     }
 
     return (
@@ -60,14 +66,19 @@ const ReviewSection: FC = () => {
             <h1 className="menuTitle1">Upload your video</h1>
             <label className="upload-zone">
                 <img src="uploadBtn.svg" alt="Upload logo"/>
-                <input className="upload-place" type="file" onChange={(e: any) => {setFile(e.target.files[0])}}/>
+                <input className="upload-place" type="file" onChange={(e: any) => {setFile(e.target.files[0]); 
+																					if(e.target.files[0] !== undefined && e.target.files[0] !== null) 
+																						review.filename = e.target.files[0].name}}/>																				
                 <span className="textClick">Click or drag file to this area to upload{displayFileChoosen(file)}</span>
                 <span className="textComm">You can upload a maximum of one hour of video</span>
             </label>
             <div className="line"></div>
             <h1 className="titleReview1">Get Your Review !</h1>
             <p className="paraReview">Book a meeting with our team to review your results</p>
-            <form onSubmit={(e) => sendEmail(e)}>
+			<form onSubmit={(e) => sendEmail(e)}>
+				<div style={displayNone}> 
+					<input type="hidden" value={review.filename} name="filename"/>
+				</div>
                 <div className="name-input">
                     <Input value={review.name} onChange={(e) => changeText(e.currentTarget.value, 'name')} placeholder="Name" className="text-input" name="name" />
                 </div>
